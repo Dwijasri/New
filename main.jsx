9faf19e37191ae7fb08f16444c6dddb861d76104
Dwijasri <157499031+Dwijasri@users.noreply.github.com>
2025-03-11 10:48:46 +0530

    Add files via upload

---
+++ main.jsx
@@ -0,0 +1,9 @@
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import './index.css'
+import App from './App.jsx'
+import Mobiles from './components/Mobiles.jsx'
+
+createRoot(document.getElementById('root')).render(
+  <Mobiles></Mobiles>,
+)
